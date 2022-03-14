337f8a8430f579ca53a8263952e739924033ec2d
Matúš Švantner <xsvantner@stuba.sk>
2022-03-14 14:03:11 +0100

    Array

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,13 +1,26 @@
 package sk.stuba.fei.uim.oop;
 
-import java.util.Objects;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String a = "a";
-        String b = "a";
+        int x =5;
+        int [] y = new int[]{1,2,3};
 
-        System.out.println("Object.equals(a,a):" + Objects.equals(a,a));
-        System.out.println("Object.equals(a,b):" + Objects.equals(a,b));
+        System.out.println(x);
+        testPrimitive(x);
+        System.out.println(x);
+
+        System.out.println(Arrays.toString(y));
+        testArray(y);
+        System.out.println(Arrays.toString(y));
+    }
+    public static void testPrimitive(int cislo){
+        cislo = 10;
+        System.out.println(cislo);
+    }
+    public  static void testArray(int[] array){
+        array[0] = 10;
+        System.out.println(Arrays.toString(array));
     }
 }
