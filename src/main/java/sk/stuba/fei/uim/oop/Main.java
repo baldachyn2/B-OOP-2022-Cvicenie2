79cfd5b63ef0fd9db1cb2149dab1cde4f8c841e1
Matúš Švantner <xsvantner@stuba.sk>
2022-03-14 14:09:39 +0100

    spread operator

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int x =5;
-        int [] y = new int[]{1,2,3};
+        int x = 5;
+        int[] y = new int[]{1, 2, 3};
 
         System.out.println(x);
         testPrimitive(x);
@@ -14,13 +14,22 @@ public class Main {
         System.out.println(Arrays.toString(y));
         testArray(y);
         System.out.println(Arrays.toString(y));
+
+        testArraySpread("text", 1,2, 3, 4, 5);
     }
-    public static void testPrimitive(int cislo){
+
+    public static void testPrimitive(int cislo) {
         cislo = 10;
         System.out.println(cislo);
     }
-    public  static void testArray(int[] array){
+
+    public static void testArray(int[] array) {
         array[0] = 10;
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void testArraySpread(String text,int... array) {
+        System.out.println(text);
         System.out.println(Arrays.toString(array));
     }
 }
