dc84938e36e3756e0d857d957aeb61d2016968cb
Matúš Švantner <xsvantner@stuba.sk>
2022-03-14 13:51:17 +0100

    Objects.equals()

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,13 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
+        String a = "a";
+        String b = "a";
 
+        System.out.println("Object.equals(a,a):" + Objects.equals(a,a));
+        System.out.println("Object.equals(a,b):" + Objects.equals(a,b));
     }
 }
