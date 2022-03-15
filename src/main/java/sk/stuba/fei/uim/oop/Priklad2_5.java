6d6533c93f87fcb0bdea3f303b31bab4d8336a48
Matúš Švantner <xsvantner@stuba.sk>
2022-03-15 19:16:37 +0100

    priklad2.5 dvojrozmerne pole

---
+++ src/main/java/sk/stuba/fei/uim/oop/Priklad2_5.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop;
+
+public class Priklad2_5 {
+    public static void main(String[] args) {
+        int[][] array2d = {{1, 2}, {3, 4}};
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(array2d[i][j]);
+            }
+        }
+    }
+}
+
