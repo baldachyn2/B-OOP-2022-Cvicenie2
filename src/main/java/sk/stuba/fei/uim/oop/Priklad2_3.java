f18e59747ce653e3571996655f4cafdb8286ff8f
Matúš Švantner <xsvantner@stuba.sk>
2022-03-15 18:45:52 +0100

    priklad3.3 array initialization

---
+++ src/main/java/sk/stuba/fei/uim/oop/Priklad2_3.java
@@ -0,0 +1,10 @@
+package sk.stuba.fei.uim.oop;
+
+public class Priklad2_3 {
+    public static void main(String[] args){
+        int[] array = new int[]{1,2,3,4,5};
+        for (int i = 0; i < 5;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
